ba9c5d142233652ce082d1572697e455bc043941
gourab <gourab.saheb100@gmail.com>
2022-06-22 20:44:01 +0530

    line

---
+++ LineComparisonOOPS/Line.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonOOPS
+{
+    public class Line
+    {
+        public double lengthofline()
+        {
+           int x1=Convert.ToInt16(Console.ReadLine());
+           int y1=Convert.ToInt16(Console.ReadLine());
+           int x2=Convert.ToInt16(Console.ReadLine());
+           int y2 =Convert.ToInt16(Console.ReadLine());
+
+            double lengthofline = (double)Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+            return (double)lengthofline;
+        }
+
+
+        
+
+    }
+}
---
+++ LineComparisonOOPS/Program.cs
@@ -0,0 +1,25 @@
+﻿using LineComparisonOOPS;
+
+
+
+ 
+    Line line = new Line();
+
+    double line1 = line.lengthofline();
+    double line2 = line.lengthofline();
+
+    Console.WriteLine(line1);
+    Console.WriteLine(line2);
+
+    if (line1 == line2)
+    {
+        Console.WriteLine("to equal");
+    }
+    else if (line1 < line2)
+    {
+        Console.WriteLine("line2 greater then line1");
+    }
+    else
+    {
+        Console.WriteLine("line2 is less");
+    }
